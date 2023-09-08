a8438ae1c68aa2a09df5e2a179a1e5fee444632a
蜡笔小开心❤️ <87791315+514420887wzr@users.noreply.github.com>
2023-09-08 11:50:42 +0800

    更换启动页

--- src/layout/welcome/index.jsx
+++ src/layout/welcome/index.jsx
@@ -1,36 +1,52 @@
 import React, { useState, useEffect } from 'react'
-import './index.scss'
-import { Layout } from 'antd'
-import {Typer} from './typer'
+import './index1.scss'
+import $ from 'jquery'
 const WelcomeLayout = props => {
   function redirect() {
     props.history.push('/home')
   }
+  useEffect(() => {
+    $(document).ready(function () {
+      console.log(`执行`, $(document));
+      let startdelay = 200;
+      let coredelay = 300;
+      let maindelay = 630;
+      setTimeout(() => {
+        $('#ringgroup').addClass('pass')
+        $('#light').addClass('pass')
+        $('#core').addClass('pass')
+      }, startdelay)
+      setTimeout(() => {
+        $('#light').addClass('passed')
+        $('#wave').addClass('pass')
+      }, startdelay + coredelay)
+      setTimeout(() => {
+        $('#body').removeClass('out').addClass('in')
+      }, startdelay + coredelay + maindelay)
+    });
+  }, []);
   return (
-    <Layout style={{ width: '100%', height: '100%' }} >
-      <div class='container' onClick={redirect}>
-
-        {/* <div class='text-container'><div></div></div> */}
-        <Typer/>
-        <div class='bird-container bird-container--one'>
-          <div class='bird bird--one'></div>
+    <div id='body' className="out">
+      <div id="light"></div>
+      <div id="wave"></div>
+      <div id="stage">
+        <div id="hero" onClick={redirect}>
+          <img src={require('../../assets/images/未标题-3.png')} alt=''></img>
+          <p className='flash'>
+            点击进入
+          </p>
         </div>
-
-        <div class='bird-container bird-container--two'>
-          <div class='bird bird--two'></div>
-        </div>
-
-        <div class='bird-container bird-container--three'>
-          <div class='bird bird--three'></div>
-        </div>
-
-        <div class='bird-container bird-container--four'>
-          <div class='bird bird--four'></div>
-        </div>
-
       </div>
-    </Layout>
-
+      <div id="ringgroup">
+        <div class="ring"></div>
+        <div class="ring"></div>
+        <div class="ring"></div>
+        <div class="ring"></div>
+      </div>
+      <div id="core">
+        <img src={require('../../assets/images/0211.png')} alt=''></img>
+      </div>
+    </div>
   )
 }
 export default WelcomeLayout
