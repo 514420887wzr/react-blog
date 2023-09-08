738c891204165fa4f8ba1421e1bf9d053613c0a7
蜡笔小开心❤️ <87791315+514420887wzr@users.noreply.github.com>
2023-09-08 16:25:02 +0800

    修复线上图片显示异常bug，增加飞机大战demo

--- src/layout/welcome/index.jsx
+++ src/layout/welcome/index.jsx
@@ -7,23 +7,28 @@ const WelcomeLayout = props => {
   }
   useEffect(() => {
     $(document).ready(function () {
-      console.log(`执行`, $(document));
+      // console.log(`执行`, $(document));
       let startdelay = 200;
       let coredelay = 300;
       let maindelay = 630;
-      setTimeout(() => {
+      let Timeout1 = setTimeout(() => {
         $('#ringgroup').addClass('pass')
         $('#light').addClass('pass')
         $('#core').addClass('pass')
       }, startdelay)
-      setTimeout(() => {
+      let Timeout2 = setTimeout(() => {
         $('#light').addClass('passed')
         $('#wave').addClass('pass')
       }, startdelay + coredelay)
-      setTimeout(() => {
+      let Timeout3 = setTimeout(() => {
         $('#body').removeClass('out').addClass('in')
       }, startdelay + coredelay + maindelay)
     });
+    return () => {
+      clearTimeout(Timeout1);
+      clearTimeout(Timeout2);
+      clearTimeout(Timeout3);
+    }
   }, []);
   return (
     <div id='body' className="out">
@@ -31,7 +36,7 @@ const WelcomeLayout = props => {
       <div id="wave"></div>
       <div id="stage">
         <div id="hero" onClick={redirect}>
-          <img src={require('../../assets/images/未标题-3.png')} alt=''></img>
+          <img src={require('../../assets/images/holle.png')} alt=''></img>
           <p className='flash'>
             点击进入
           </p>
